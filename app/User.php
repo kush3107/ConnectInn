2baa333e331e78707b45b8475d66ce607588b8b0
Kushagra <kkss420@gmail.com>
2017-11-04 12:51:57 +0530

    added follower migration and realtionship method

--- app/User.php
+++ app/User.php
@@ -77,4 +77,11 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    // Relationship Methods
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id')->withTimestamps();
+    }
 }
