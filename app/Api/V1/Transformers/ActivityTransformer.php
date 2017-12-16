02445f0230b5e6ac93fb365893fd501ddcc78253
Kushagra <kkss420@gmail.com>
2017-12-17 01:17:14 +0530

    changed the includes in activity transformer;

--- app/Api/V1/Transformers/ActivityTransformer.php
+++ app/Api/V1/Transformers/ActivityTransformer.php
@@ -15,32 +15,40 @@ use League\Fractal\TransformerAbstract;
 
 class ActivityTransformer extends TransformerAbstract
 {
-    protected $defaultIncludes = ['owner', 'members'];
+    protected $availableIncludes = ['owner', 'members'];
+    protected $defaultIncludes = ['tags'];
 
     public function transform(Activity $activity)
     {
         $status = is_null($activity->end) ? 'ongoing' : 'finished';
 
         return [
-            'id'            => (int)$activity->id,
-            'type'          => $activity->type,
-            'title'         => $activity->title,
-            'description'   => $activity->description,
-            'start'         => Helpers::nullOrDateTimeString($activity->start),
-            'end'           => Helpers::nullOrDateTimeString($activity->end),
-            'status'        => $status,
-            'link'          => $activity->link,
-            'meta'          => json_decode($activity->meta),
-            'created_at'    => $activity->created_at->toDateTimeString(),
-            'updated_at'    => $activity->updated_at->toDateTimeString(),
+            'id' => (int)$activity->id,
+            'type' => $activity->type,
+            'title' => $activity->title,
+            'description' => $activity->description,
+            'start' => Helpers::nullOrDateTimeString($activity->start),
+            'end' => Helpers::nullOrDateTimeString($activity->end),
+            'status' => $status,
+            'link' => $activity->link,
+            'meta' => json_decode($activity->meta),
+            'created_at' => $activity->created_at->toDateTimeString(),
+            'updated_at' => $activity->updated_at->toDateTimeString(),
         ];
     }
 
-    public function includeOwner(Activity $activity) {
+    public function includeOwner(Activity $activity)
+    {
         return $this->item($activity->owner()->first(), new UserTransformer());
     }
 
-    public function includeMembers(Activity $activity) {
+    public function includeMembers(Activity $activity)
+    {
         return $this->collection($activity->members, new UserTransformer());
+    }
+
+    public function includeTags(Activity $activity)
+    {
+        return $this->collection($activity->tags, new TagTransformer());
     }
 }
